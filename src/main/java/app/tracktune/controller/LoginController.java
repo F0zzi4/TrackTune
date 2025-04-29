89b719bb68188e4ac025db03b0903b90eb33c4e4
f0zzi4 <fozzatodavide@gmail.com>
2025-04-29 11:05:09 +0200

    aggiornamento per materialfx

--- src/main/java/app/tracktune/controller/LoginController.java
+++ src/main/java/app/tracktune/controller/LoginController.java
@@ -1,27 +1,57 @@
 package app.tracktune.controller;
 
+import app.tracktune.exceptions.UserAlreadyExistsException;
+import app.tracktune.model.user.User;
+import app.tracktune.model.user.UserDAO;
 import app.tracktune.utils.Strings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class LoginController {
+    private final UserDAO userDAO;
+    private Alert errorAlert;
+
+    public LoginController() {
+        userDAO = new UserDAO();
+    }
+
     @FXML
     private TextField TxtEmail;
     @FXML
     private TextField TxtPassword;
     @FXML
     private void handleLogin() {
-        String username = TxtEmail.getText();
-        String password = TxtPassword.getText();
+        try{
+            String username = TxtEmail.getText();
+            String password = TxtPassword.getText();
 
-        if (username.isEmpty() || password.isEmpty()) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle(Strings.LOGIN_FAILED);
-            alert.setHeaderText(Strings.ERROR);
-            alert.setContentText(Strings.USER_PWD_EMPTY);
-            alert.showAndWait();
+            if(isInputValid(username, password)){
+                User user = userDAO.getUser(username);
+                if(user != null){
+                    userDAO.saveUser(User.create(username, password));
+                }
+                throw new UserAlreadyExistsException(Strings.ERR_USER_ALREADY_EXISTS);
+            }
+        }catch(RuntimeException e){
+            setAlert(Strings.LOGIN_FAILED, Strings.ERROR, e.getMessage());
         }
+    }
 
+    private boolean isInputValid(String username, String password){
+        boolean result = true;
+        if (username.isEmpty() || password.isEmpty()) {
+            setAlert(Strings.LOGIN_FAILED, Strings.ERROR, Strings.USER_PWD_EMPTY);
+            result = false;
+        }
+        return result;
+    }
+
+    private void setAlert(String title, String header, String content){
+        errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setTitle(title);
+        errorAlert.setHeaderText(header);
+        errorAlert.setContentText(content);
+        errorAlert.showAndWait();
     }
 }
