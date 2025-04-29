1403b79d89ef84ae9f32af0fde89300c4c99d2c3
f0zzi4 <fozzatodavide@gmail.com>
2025-04-29 11:06:32 +0200

    aggiornamento per materialfx

--- src/main/java/app/tracktune/exceptions/UserAlreadyExistsException.java
+++ src/main/java/app/tracktune/exceptions/UserAlreadyExistsException.java
@@ -1,6 +1,5 @@
 package app.tracktune.exceptions;
 
 public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException() {}
     public UserAlreadyExistsException(String message) {}
 }
