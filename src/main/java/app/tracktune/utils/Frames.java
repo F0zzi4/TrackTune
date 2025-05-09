71a8ef1f12d7f71e58dc27f943d7fed31c6a7ce8
MattiaRebo <mattia.rebonato@gmail.com>
2025-05-09 15:49:39 +0200

    Sistemazioni varia

--- src/main/java/app/tracktune/utils/Frames.java
+++ src/main/java/app/tracktune/utils/Frames.java
@@ -12,7 +12,7 @@ public class Frames {
     // DASHBOARD
     public static final int DASHBOARD_FRAME_WIDTH = 1200;
     public static final int DASHBOARD_FRAME_HEIGHT = 800;
-    public static final String DASHBOARD_VIEW_PATH = "/fxml/DashboardView.fxml";
+    public static final String DASHBOARD_VIEW_PATH = "/fxml/admin/DashboardView.fxml";
     public static final String REQUEST_VIEW_PATH = "/fxml/AccountRequestView.fxml";
     // FRAME ICONS
     public static final String MAIN_ICON_PATH = "/assets/icon.png";
